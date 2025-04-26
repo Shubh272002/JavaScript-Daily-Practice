e7ff1ac9286d660a2ad81967e2c44488ec8db855
Shubh Verma <shubhvermaswe@gmail.com>
2025-04-26 15:22:20 +0530

    Add files via upload

---
+++ Array Methods -2 Practice/data.js
@@ -0,0 +1,7 @@
+var userid,username,dept,desig,salary,image;
+userid=[120,121,122,123];
+username=["Ram","Mohan","karan","Suresh"];
+dept=["Sales","Production","Finishing","Accounts"];
+desig=["Manager","Supervisor","Cleark","Helper"];
+salary=[12000,13000,14000,15000];
+image=['p1.jpg','p2.jpg','p3.jpg','p4.jpg'];
